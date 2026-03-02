c9f8310571b90c8b6761b1b1bbc22827f2911f37
Ashwani Kumar Mishra <mfsi.ashwanim@gmail.com>
2026-03-02 16:42:03 +0530

    jenkins file added

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,11 +13,6 @@ import { defineConfig, devices } from '@playwright/test';
  */
 
 export default defineConfig({
-  reporter : [
-       ['json' , {outputfile : 'testresults/jsonreport.json'}],
-       ['junit' , {outputfile : 'testresults/junitreport.junit'}],
-       ['allure-playwright' , {outputfolder : 'testresults'}],
-    ],
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -27,6 +22,11 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 1,
+  reporter : [
+       ['json' , {outputfile : 'testresults/jsonreport.json'}],
+       ['junit' , {outputfile : 'testresults/junitreport.junit'}],
+       ['allure-playwright' , {outputfolder : 'testresults'}],
+    ],
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
  // reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
